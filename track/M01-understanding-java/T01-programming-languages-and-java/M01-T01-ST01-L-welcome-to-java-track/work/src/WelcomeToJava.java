cdc4a18a61f4b68e68fe1cb84e1f457d9eef275b
AHAMAD ALISHA SHAIK <shaikahamadalisha6865@gmail.com>
2026-07-30 12:59:29 +0530

    added one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -5,6 +5,6 @@ public class WelcomeToJava {
         System.out.println("Welcome to The java track");
         System.out.println("I already know how to build programing logic");
         System.out.println("Now I am learning how to express that logic using java");
-
+        System.out.println("now i know how to use git and github");
     }
 }
