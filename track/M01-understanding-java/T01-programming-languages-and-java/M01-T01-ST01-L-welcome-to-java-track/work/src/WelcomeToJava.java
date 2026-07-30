e502466b792dd274f01679bfea8d0189847fc1a7
AHAMAD ALISHA SHAIK <shaikahamadalisha6865@gmail.com>
2026-07-30 12:45:07 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,10 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("Welcome to The java track");
+        System.out.println("I already know how to build programing logic");
+        System.out.println("Now I am learning how to express that logic using java");
+
     }
 }
