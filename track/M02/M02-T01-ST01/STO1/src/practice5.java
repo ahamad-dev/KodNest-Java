896ca7bde889d620011ed9d5ba05bd24142d88e2
AHAMAD ALISHA SHAIK <shaikahamadalisha6865@gmail.com>
2026-08-04 10:28:00 +0530

    latest push

--- track/M02/M02-T01-ST01/STO1/src/practice5.java
+++ track/M02/M02-T01-ST01/STO1/src/practice5.java
@@ -1,9 +1,10 @@
 public class practice5 {
-public static void main(String[] args) {
-System.out.println("---------------------");
-System.out.println("STUDENT PROFILE");
-System.out.println("Name: Alex");
-System.out.println("Course: Java");
-System.out.println("Level: Beginner");
-System.out.println("----------------------");
+    public static void main(String[] args) {
+        System.out.println("---------------------");
+        System.out.println("STUDENT PROFILE");
+        System.out.println("Name: Alex");
+        System.out.println("Course: Java");
+        System.out.println("Level: Beginner");
+        System.out.println("----------------------");
+    }
 }
