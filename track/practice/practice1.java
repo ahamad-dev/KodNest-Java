602ea78dcc2cd2433d6063d934098c526ee39b0b
AHAMAD ALISHA SHAIK <shaikahamadalisha6865@gmail.com>
2026-08-04 11:32:16 +0530

    push

---
+++ track/practice/practice1.java
@@ -0,0 +1,11 @@
+public class practice6 {
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("wednesday");
+        System.out.println("Thursday");
+        System.out.print("friday");
+        System.out.println("saturday");
+        System.out.println("sunday");
+    }
+}
