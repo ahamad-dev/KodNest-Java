4f3f4506ef8fe875ce4374aba61f580af1ea6c58
AHAMAD ALISHA SHAIK <shaikahamadalisha6865@gmail.com>
2026-08-04 12:26:42 +0530

    push

---
+++ track/practice/practice2.java
@@ -0,0 +1,12 @@
+public class practice2 {
+    public static void main(String[] args) {
+        int num = 25;
+        if (num == 0) {
+            System.out.println("num is 0");
+        } else if (num > 0) {
+            System.out.println("num is positive");
+        } else {
+            System.out.println("num is negative");
+        }
+    }
+}
