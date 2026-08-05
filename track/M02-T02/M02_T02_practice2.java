3e90a1dedfabed2b33258b0e1919197655911f16
AHAMAD ALISHA SHAIK <shaikahamadalisha6865@gmail.com>
2026-08-05 12:31:31 +0530

    push

---
+++ track/M02-T02/M02_T02_practice2.java
@@ -0,0 +1,10 @@
+public class M02_T02_practice2 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
